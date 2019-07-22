4e2b91c7acef788bd6bf57a58283ec9eb75b20c2
Adam Recvlohe <adam.recvlohe@pm.me>
2019-07-22 23:57:10 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Muscogee Creek Dictionary
+
+## Summary
+
+The purpose of this application is to help Muscogee Language learners search for terms more easily and to be empowered to speak mvskoke emponvkv.
+
+## Stack
+
+- FE - ReasonML, ReasonReact, TailwindCSS
+- BE - Express/Node
+- Hosting - DigitalOcean
+
+## Getting Started
+
+To run this project locally follow these steps from the command line:
+
+1. Clone the repositry
+
+```shell
+git clone https://github.com/nativesintech/muscogee-creek-dictionary.git
+```
+
+2. Go into directory
+
+```shell
+cd muscogee-creek-dictionary
+```
+
+3. Install dependencies
+
+```shell
+yarn install
+```
+
+4. Start the project in development mode
+
+```shell
+yarn start
+```
+
+## Contributing
+
+Natives in Tech would love for you to contribute to the project. We accept all kinds of pull requests. Please read our [Code of Conduct](./CODE_OF_CONDUCT.md) so you are aware of the ground rules for engaging with others in this repository.
---
+++ webpack/webpack.config.prod.js
@@ -0,0 +1,75 @@
+const path = require("path");
+const glob = require("glob");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ManifestPlugin = require("webpack-manifest-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+const { StatsWriterPlugin } = require("webpack-stats-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "../src/client.js"),
+  output: {
+    filename: "[name].[hash].js",
+    path: path.join(__dirname, "../dist"),
+    publicPath: "/assets/"
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-syntax-dynamic-import"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash].css",
+      chunkFilename: "[id].[hash].css"
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync(`./lib/js/src/**/*`, {
+        nodir: true
+      }),
+      whitelist: ["html", "body", "root"],
+      extractors: [
+        {
+          extractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+          extensions: ["js", "html"]
+        }
+      ],
+      rejected: true
+    }),
+    new ManifestPlugin(),
+    new StatsWriterPlugin({
+      filename: "stats.json"
+    })
+  ],
+  optimization: {
+    minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin()],
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: "styles",
+          test: /\.css$/,
+          chunks: "all",
+          enforce: true
+        }
+      }
+    }
+  }
+};
